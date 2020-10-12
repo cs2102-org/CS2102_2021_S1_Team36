d9bcf896b6217fb6e9312b170808173a924b3f30
Yonggiee <fongyongjie27@gmail.com>
2020-10-12 13:22:38 +0800

    create dummy sql table and values for template

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const pool = require('./sql');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.get('/', async(req, res) => {
+    try {
+        const test = await pool.query('SELECT * FROM test');
+        console.log(test);
+        res.json(test.rows);
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 app.use(express.json())
     .use(express.urlencoded({extended: false}))
---
+++ backend/sql_init/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE pcs;
+
+\c pcs;
+
+CREATE table test (
+    a integer
+);
+
+INSERT INTO test VALUES (1);
