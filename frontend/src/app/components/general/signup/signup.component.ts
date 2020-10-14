a6734cbeb9d37cbd71c65e5a92c482d436b22b18
Yonggiee <fongyongjie27@gmail.com>
2020-10-14 19:42:28 +0800

    create skeletal signup page

--- frontend/src/app/components/general/signup/signup.component.ts
+++ frontend/src/app/components/general/signup/signup.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidatorFn, FormGroup, ValidationErrors, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  passwordMatchValidator: ValidatorFn = (
+    control: FormGroup
+  ): ValidationErrors | null => {
+    const password = control.get('password');
+    const passwordConfirm = control.get('password_confirm');
 
-  constructor() { }
+    return password && passwordConfirm &&
+      password.value === passwordConfirm.value ? null : { notMatched: true };
+  };
 
-  ngOnInit(): void {
+  signUpForm = new FormGroup(
+    {
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      password_confirm: new FormControl(''),
+    },
+    { validators: this.passwordMatchValidator }
+  );
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    const signUpDetails = this.signUpForm.value;
   }
-
 }
