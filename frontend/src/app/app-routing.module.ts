425e2eb4b3a8b7ed4a1c9ee08fc32cf67e12ffce
Yonggiee <fongyongjie27@gmail.com>
2020-10-14 14:50:42 +0800

    add caretaker availability page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/general/home/home.component';
+import { CareTakerAvailabilityPageComponent } from './components/pet-owner/care-taker-availability-page/care-taker-availability-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'caretaker-availabilities', component: CareTakerAvailabilityPageComponent },
 ];
 
 @NgModule({
@@ -13,4 +15,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [HomeComponent];
+export const routingComponents = [HomeComponent, CareTakerAvailabilityPageComponent];
---
+++ frontend/src/app/components/pet-owner/care-taker-availability-page/care-taker-availability-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-care-taker-availability-page',
+  templateUrl: './care-taker-availability-page.component.html',
+  styleUrls: ['./care-taker-availability-page.component.css']
+})
+export class CareTakerAvailabilityPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
