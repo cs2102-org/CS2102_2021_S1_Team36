eb953c3ff4232165456faf25e9aaadaa23efb4e3
Yonggiee <fongyongjie27@gmail.com>
2020-10-14 14:48:09 +0800

    add menu header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuHeaderComponent } from './components/general/menu-header/menu-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    MenuHeaderComponent
   ],
   imports: [
     BrowserModule,
