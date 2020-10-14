48a13da665988460a97a1d65b85fa2ac6b27a6ce
Yonggiee <fongyongjie27@gmail.com>
2020-10-14 14:54:17 +0800

    add signup and login components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuHeaderComponent } from './components/general/menu-header/menu-header.component';
+import { LoginComponent } from './components/general/login/login.component';
+import { SignupComponent } from './components/general/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    MenuHeaderComponent
+    MenuHeaderComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
