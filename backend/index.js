a6c33c0893c5df2229da6f1f5d8fd86c4fd79668
Yonggiee <fongyongjie27@gmail.com>
2020-10-20 10:31:16 +0800

    add user login and signup endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const pool = require('./sql');
+const pool = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', async(req, res) => {
-    try {
-        const test = await pool.query('SELECT * FROM test');
-        console.log(test);
-        res.json(test.rows);
-    } catch (err) {
-        console.error(err);
-    }
-});
+// app.get('/', async(req, res) => {
+//     try {
+//         const test = await pool.query('SELECT * FROM test');
+//         console.log(test);
+//         res.json(test.rows);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// });
+
+const usersRouter = require('./routes/users');
 
 app.use(express.json())
     .use(express.urlencoded({extended: false}))
+    .use('/api', usersRouter)
     .listen(PORT, () => console.log(`Running server on ${PORT}`));
