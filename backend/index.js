51ff73b5a8ee9748fbfe7efda2db004fff6fac3d
Li Zi Ying <ziying338@gmail.com>
2020-10-22 19:20:07 +0800

    Add set up and pcs admin route

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
+require('express-async-errors');
+require('dotenv');
 const express = require('express');
 const pool = require('./sql');
+const {pcsRouter} = require('./routes/pcsadmin.js');
 
+const router = express.Router();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// DEFINE ROUTES
+router.use("/", pcsRouter);
 
 app.get('/', async(req, res) => {
     try {
@@ -16,4 +23,5 @@ app.get('/', async(req, res) => {
 
 app.use(express.json())
     .use(express.urlencoded({extended: false}))
+    .use(router)
     .listen(PORT, () => console.log(`Running server on ${PORT}`));
