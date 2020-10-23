3844c8f71ef62e9eb5c4fcf0ca9cf13bfd8d812c
Li Zi Ying <ziying338@gmail.com>
2020-10-23 09:05:38 +0800

    Update route path

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // DEFINE ROUTES
-router.use("/", pcsRouter);
+router.use("/pcs-admins", pcsRouter);
 
 app.get('/', async(req, res) => {
     try {
