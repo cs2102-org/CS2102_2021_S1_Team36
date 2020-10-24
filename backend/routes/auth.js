127a0eae8049989e404a38eaf2add46acb274537
Yonggiee <fongyongjie27@gmail.com>
2020-10-24 18:00:51 +0800

    remove bcrypt for the time being

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -19,16 +19,18 @@ router.get("/", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const { rows } = await pool.query(
-    "SELECT password, is_fulltime, U.email as uemail, P.email as pemail, C.email as cemail A.email as aemail from ((Users U LEFT JOIN PetOwners P ON U.email=P.email) LEFT JOIN Caretakers C ON U.email=C.email) LEFT JOIN PCSAdmins A on U.email=A.email WHERE U.email=$1;"
-  , [email]);
+    "SELECT password, is_fulltime, U.email AS uemail, P.email AS pemail, C.email AS cemail, A.email AS aemail \
+      FROM ((Users U LEFT JOIN PetOwners P ON U.email=P.email) \
+        LEFT JOIN Caretakers C ON U.email=C.email) \
+          LEFT JOIN PCSAdmins A on U.email=A.email \
+    WHERE U.email=$1 AND U.password=$2"
+  , [email, password]);
 
   if (rows.length > 0) {
     const user = rows[0];
-    const passwordStored = user.password;
-    console.log(passwordStored);
-    console.log(password);
-    const validPass = await bcrypt.compare(password, passwordStored);
-    if (validPass) {
+    // const passwordStored = user.password;
+    // const validPass = await bcrypt.compare(password, passwordStored);
+    // if (validPass) {
       return jwt.sign(rows[0], 'secretkey', (err, token) => {
         const returnJson = { token };
         if (user.pemail != null) {
@@ -41,10 +43,9 @@ router.post("/login", async (req, res) => {
         } if (user.aemail != null) {
           returnJson['is_admin'] = true;
         } 
-        console.log(returnJson);
         return res.status(200).json(returnJson);
       });
-    }
+    // }
   } 
 
   return res.status(404).json({ error: "User not found" });
@@ -53,11 +54,11 @@ router.post("/login", async (req, res) => {
 // User signup
 router.post("/signup", async (req, res) => {
   const { name, email, password, desc, caretaker, pet_owner, type } = req.body;
-  const hash = await bcrypt.hash(password, saltRounds);
+  // const hash = await bcrypt.hash(password, saltRounds);
   try {
     await pool.query(
       "INSERT INTO Users VALUES ($1, $2, $3, $4);"
-    , [name, email, hash, desc]);
+    , [name, email, desc, password]);
   } catch (e) {
     return res.status(404).json({ error: e.toString() });
   }
