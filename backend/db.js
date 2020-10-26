3b908827b5c9ce6f10a931f659f0b5b502fafec0
pangjiahao <dontaskmestuff@gmail.com>
2020-10-26 13:59:10 +0800

    ignore db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    database: "pcs",
-    host: "localhost",
-    password: "aaa",
-    port: "5432"
+    user: process.env.PSQL_USER,
+    database: "pcs"
 });
 
 module.exports = pool;
