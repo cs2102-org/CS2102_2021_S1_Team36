39c2fd5ca595bebbb0ee12185a8cd7c864e88d88
PangJiaDa <pangjiada52@gmail.com>
2020-10-27 16:40:14 +0800

    Add endpoints: add new pet to owner and add rating for a bid

--- backend/index.js
+++ backend/index.js
@@ -47,3 +47,23 @@ app.get('/user/owns/:email', async(req, res) => {
         console.error(err);
     }
 });
+
+// Add new pet to the database of a specific user
+// email must exist in the table of Users (FK dependency)
+// pet_name must not already exist in Pets table (must be unique)
+app.post('/user/owns/:email', async (req, res) => {
+    try {
+        const { email } = req.params
+        const { pet_name, special_requirements, description, species } = req.body;
+        // console.log(req.body);
+        const sql = await pool.query(
+            "INSERT INTO Pets (email, pet_name, special_requirements, description, species) \
+             VALUES ($1, $2, $3, $4, $5);",
+             [email, pet_name, special_requirements, description, species]
+        );
+        res.status(200).send(`Inserted: ${email}, ${pet_name}, ${special_requirements}, ${description}, ${species}`);
+    } catch (err) {
+        console.error(err);
+    }
+});
+
