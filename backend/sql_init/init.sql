65d5f123c09b92904ee40cfb0ad8d3d3c8bea679
Li Zi Ying <ziying338@gmail.com>
2020-10-31 13:00:03 +0800

    Add overlap constraint triggers

--- backend/sql_init/init.sql
+++ backend/sql_init/init.sql
@@ -665,10 +665,63 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- calling the trigger
 DROP TRIGGER IF EXISTS user_cover_trigger ON Users;
 CREATE CONSTRAINT TRIGGER user_cover_trigger
     AFTER INSERT ON Users
     DEFERRABLE INITIALLY DEFERRED
     FOR EACH ROW
     EXECUTE PROCEDURE check_user_covering();
+
+-- admin + petowner overlap constraint
+CREATE OR REPLACE FUNCTION check_admin_petowner_overlap() RETURNS TRIGGER
+    AS $$
+DECLARE 
+    overlap_user VARCHAR(30);
+BEGIN
+    SELECT pcs.email into overlap_user
+    FROM PcsAdmins pcs, PetOwners p
+    WHERE pcs.email = p.email;
+
+    IF overlap_user IS NOT NULL THEN
+        RAISE exception '% should not be both PCS Admin and Pet Owner', overlap_user;
+    END IF;
+    RETURN NULL;
+END
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS petowner_overlap_trigger ON PetOwners;
+CREATE TRIGGER petowner_overlap_trigger
+    AFTER INSERT ON PetOwners
+    EXECUTE PROCEDURE check_admin_petowner_overlap();
+
+DROP TRIGGER IF EXISTS pcs_petowner_overlap_trigger ON PcsAdmins;
+CREATE TRIGGER pcs_petowner_overlap_trigger
+    AFTER INSERT ON PcsAdmins
+    EXECUTE PROCEDURE check_admin_petowner_overlap();
+
+-- admin + caretaker overlap constraint
+CREATE OR REPLACE FUNCTION check_admin_caretaker_overlap() RETURNS TRIGGER
+    AS $$
+DECLARE 
+    overlap_user VARCHAR(30);
+BEGIN
+    SELECT pcs.email into overlap_user
+    FROM PcsAdmins pcs, CareTakers c
+    WHERE pcs.email = c.email;
+
+    IF overlap_user IS NOT NULL THEN
+        RAISE exception '% should not be both PCS Admin and CareTaker', overlap_user;
+    END IF;
+    RETURN NULL;
+END
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS caretaker_overlap_trigger ON CareTakers;
+CREATE TRIGGER caretaker_overlap_trigger
+    AFTER INSERT ON CareTakers
+    EXECUTE PROCEDURE check_admin_caretaker_overlap();
+
+DROP TRIGGER IF EXISTS pcs_caretaker_overlap_trigger ON PcsAdmins;
+CREATE TRIGGER pcs_caretaker_overlap_trigger
+    AFTER INSERT ON PcsAdmins
+    EXECUTE PROCEDURE check_admin_caretaker_overlap();
